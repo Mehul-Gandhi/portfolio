9380f3702bce7dac40914a5386626ed2fe085cf0
Mehul-Gandhi <gandhi@berkeley.edu>
2023-08-18 01:17:14 -0700

    Add more classes

--- src/Projects.js
+++ src/Projects.js
@@ -91,7 +91,7 @@ export default function Projects() {
         {name: "Gitlet", class: cs61b, description: "A version control system in Java that mimics some of the basic features of Git such as add, commit, remote push, remote pull, merge, and checkout.", details: ["Built a version control system in Java that mimics some of the basic features of Git such as add, commit, remote push, remote pull, merge, and checkout.",
         "Utilized file management, serialization, and extensive error checking and handling with comprehensive unit tests and integration tests using JUnit library"], skills: ["Java", "Systems Design"],
     links: [{name: "Request Access", content: mail}, {name: "Project Spec", content: "https://inst.eecs.berkeley.edu/~cs61b/fa21/materials/proj/proj3/index.html"}],
-    images: ["/gitlet/git.png", "/gitlet/process.png", "/gitlet/split.png"], date: "2021-12-01"}, 
+    images: ["/gitlet/git.png", "/gitlet/process.png", "/gitlet/split.png"], date: "2021-12-06"}, 
     {name: "Enigma", class: cs61b, description: "Replicated the WWII German encryption machine 'Enigma' by building a generalized simulator that could handle numerous different descriptions of possible initial configurations of the machine and messages to encode or decode.",
 details: ["Replicated the WWII German encryption machine 'Enigma' by building a generalized simulator that could handle numerous different descriptions of possible initial configurations of the machine and messages to encode or decode.", "Built in Java and worked mostly with Java's String, HashMap, ArrayList, and Scanner data structures to handle string manipulation, data mapping required, and file reading for encryption and decryption."],
 skills: ["Java"], links: [{name: "Request Access", content: mail}, {name: "Project Spec", content: "https://inst.eecs.berkeley.edu/~cs61b/fa21/materials/proj/proj1/index.html"}], images: ["/enigma/machine.jpg", "/enigma/machine.png", "/enigma/permutation.png"], date: "2021-10-05"},
@@ -100,7 +100,18 @@ skills: ["Python", "Scheme"], links: [{name: "Request Access", content: mail}, {
 images: ["/scheme/scheme.png", "/scheme/interpreter.png", "/scheme/map.png"], date: "2021-08-01"},
  {name: "Pokemon Discord Bot", class: "Side Project", description: "A Pokemon discord bot implemented in python. Contains pokemon minigames, a simple TCG, and anime commands. Uses PostgreSQL.", details: ["A Pokemon discord bot implemented in python. Contains pokemon minigames, a simple TCG, and anime commands. Uses PostgreSQL."],
 skills: ["Python", "SQL"], links: [{name: "GitHub", content: "https://github.com/Mehul-Gandhi/pokemon-discord-bot"}, {name: "Video", content: "https://youtu.be/FvExsGKHX1M"}], 
-images: ["/discord/bag.png", "/discord/poker.png", "/discord/ttt.png"], date: "2022-03-28"}
+images: ["/discord/bag.png", "/discord/poker.png", "/discord/ttt.png"], date: "2022-03-28"},
+{name: "Shepherd", class: "Pioneers in Engineering", description: "Shepherd is the team that is in charge of field control. Shepherd brings together all the data on the game field into one centralized location, where it keeps track of score, processes game-specific actions, keeps track of time, and informs the scoreboard. I created the alliance selection menu.",
+details: [], skills: ["Python", "HTML", "CSS", "JavaScript", "Flask"], links: [{name: "GitHub", content: "https://github.com/pioneers/shepherd"}, {name: "Video", content: "https://youtu.be/htvO3EBsScM"}],
+images: ["/shepherd/creator.png","/shepherd/scoreboard.png", "/shepherd/gui.png", "/shepherd/selection.png"], date: "2023-05-01"},
+{name: "2048", class: "Side Project", description: "2048 is a single-player sliding block puzzle game designed by Italian web developer Gabriele Cirulli. The game's objective is to slide numbered tiles on a grid to combine them to create a tile with the number 2048. However, one can continue to play the game after reaching the goal, creating tiles with larger numbers. I implemented this in Python using Pygame", 
+details: [], skills: ["Python"], links: [{name: "GitHub", content: "https://github.com/Mehul-Gandhi/2048"}], images: ["2048/screenshot.png"], date: "2021-12-05"},
+{name: "Jumping Cube Game", class: cs61b, description: "The KJumpingCube game1 is a two-person board game. It is a pure strategy game, involving no element of chance. I created an AI using the minimax algorithm and alpha-beta pruning for a human player to play against the computer. I also created a GUI for the game.",
+details: [], skills: ["Java"], links: [{name: "Request Access", content: mail}, {name: "Project Spec", content: "https://inst.eecs.berkeley.edu/~cs61b/fa21/materials/proj/proj2/index.html"}, {name: "Play", content: "http://www.playonlinedicegames.com/jumpingcube"}],
+images: ["/jump/alphabeta.png", "/jump/cascadeOverfull2.png"], date: "2021-11-04"}, 
+{name: "Yelp Clone App", class: "Side Project", description: "Developed a Yelp-like website application using the MERN tech stack to enable users to explore and rate 1000+ restaurants in Malaysia, fostering an interactive environment", 
+details: [], skills: ["React", "NextJS", "Express", "MongoDB", "JavaScript", "HTML", "CSS"],
+links: [{name: "GitHub", content: "https://github.com/Mehul-Gandhi/sss"}], date: "2023-08-18", images: ["/yelp/map.png", "/yelp/list.png"]}
     ].sort((a, b) => {
         const dateA = new Date(a.date);
         const dateB = new Date(b.date);
